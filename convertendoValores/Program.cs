737b680f4642254c0e959d59692a7265c6ee4c0f
andre <andrefrateschi@gmail.com>
2020-09-02 19:14:42 -0300

    commit 01

---
+++ convertendoValores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace convertendoValores
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string nome = null;
+            int idade = 0;
+      
+
+            Console.WriteLine("Digite o seu nome: ");
+            //solicitando o nome 
+            nome = Console.ReadLine();
+
+
+            Console.WriteLine("Digite a sua idade: ");
+            //solicitando a idade  
+            idade = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Seu nome e: " + nome);
+            Console.WriteLine("Sua idade e: " + idade);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
